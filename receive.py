6d3d71da9d5d4f83d5b9604fa57882c2ca33f2c8
arianaolson419 <ariana.olson@students.olin.edu>
2016-09-19 00:12:43 -0400

    added functions for translating message to binary and back

--- receive.py
+++ receive.py
@@ -3,6 +3,34 @@ import numpy as np
 from numpy import pi
 import matplotlib.pyplot as plt
 
+def getMessage():
+	prompt = raw_input("Type a message: ")
+	return prompt
+
+def StringToBits(s):
+	asciiArray = np.array([ord(c) for c in s], dtype=np.uint8)
+	binArray = np.unpackbits(asciiArray)
+	return binArray
+
+def Modulate(binArray):
+	# todo: implement FM on message
+	pass
+
+def Demodulate():
+	# todo: implement demodulation
+	pass
+
+def BitsToString(binArray):
+	asciiArray = np.packbits(binArray)
+	asciiList = asciiArray.tolist()
+	return ''.join(chr(i) for i in asciiList)
+
+
+
+
+
+
+
 duration = 2
 Fs = 44100
 sd.default.samplerate = Fs
@@ -27,14 +55,16 @@ R = np.abs(np.fft.fftshift(np.fft.fft(recFilt)))
 fs = np.linspace(-np.pi, np.pi, len(M))
 
 if __name__ == "__main__":
-	plt.subplot(311)
-	plt.plot(fs, M)
+	# plt.subplot(311)
+	# plt.plot(fs, M)
 
-	plt.subplot(312)
-	plt.plot(t, myrecording)
+	# plt.subplot(312)
+	# plt.plot(t, myrecording)
 	
-	plt.subplot(313)
-	plt.plot(fs, R)
+	# plt.subplot(313)
+	# plt.plot(fs, R)
 
-	plt.show()
+	# plt.show()
+
+	print BitsToString(StringToBits("Ariana Olson"))
 
