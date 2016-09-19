50d5882c9c12d1e3d015fe12e41719918ebe43e1
arianaolson419 <ariana.olson@students.olin.edu>
2016-09-18 23:14:48 -0400

    playing with filtering, imported sounddevice for recording audio

--- transmit.py
+++ transmit.py
@@ -3,24 +3,29 @@ Transmitter for communications system
 Transmits an audio signal
 By Ariana Olson September 2016
 '''
-import scikits.audiolab as audiolab
+import sounddevice as sd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 #create the time vector
 Fs = 10000	#samples/second
-timeDuration = 1	#second
-t = np.linspace(0, timeDuration, Fs)
+timeDuration = 0.5	#second
+numberOfSamples = Fs * 0.5
+t = np.linspace(0, timeDuration, Fs*timeDuration)
 note = 440	# an A note
 y = np.sin(t*note*2*np.pi)
 h = np.array([0.1, -0.08, 0.06, -0.05, 0.04])
 x = np.array([1, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 1])
 
 xc = np.convolve(x, h)
+sd.default.samplerate = Fs
+sd.default.channels = 1
 
 
 if __name__ == '__main__':
 	plt.stem(xc)	#stem plot of convolved signal
-	audiolab.play(y, fs=Fs)	#play a 440Hz note
+	recording = sd.playrec(y)	#play a 440Hz note
+	plt.figure(2)
+	plt.plot(recording)
 	plt.show()
