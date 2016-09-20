dff27012b222e1432c6ac4fedf77e1afee760c44
arianaolson419 <ariana.olson@students.olin.edu>
2016-09-19 22:27:50 -0400

    added comments and docstrings

--- receive.py
+++ receive.py
@@ -1,27 +1,71 @@
+'''
+Acoustic Modem Project QEAF2016
+
+by Ariana Olson
+
+Implements Binary Phase-Shift Keying using audible frequencies to create an acoustic modem. 
+One computer is used as both the transmitter and receiver, as this program is meant to be more
+of a proof of concept than a functional communications system.
+
+When the program is run, the user is prompted to type in a short message. The message is then translated into binary bits,
+modulated into an audible signal, and demodulated and translated back to text. 
+
+WARNING: it is best to keep messages to one short word in length as an audble, and not 
+very pleasant, signal is produced, which can be extremely annoying to yourself and your neighbors.
+'''
 import sounddevice as sd
 import numpy as np
 from numpy import pi
 import matplotlib.pyplot as plt
 
 def getMessage():
-	prompt = raw_input("Type a message: ")
-	return prompt
+	'''
+	Prompts the user to type in a short message that will be transmitted via an audio signal
+
+	returns the message as a string
+	'''
+	message = raw_input("Type a message: ")
+	return message
 
 def StringToBits(message):
+	'''
+	Converts a string to its binary representation
+
+	message: a string
+
+	returns a numpy array of binary bits (8 bits per character)
+	'''
 	asciiArray = np.array([ord(c) for c in message], dtype=np.uint8)
 	binArray = np.unpackbits(asciiArray)
+
 	return binArray
 
 def Modulate(binArray, Fs, fc, bitTime = .25):
-	numberOfBits = len(binArray)	# number of zeros and ones in array
-	duration = bitTime * numberOfBits	# time it takes to play the audio signal
-	signalArray = np.ones(int(duration * Fs))	# initialze the signal array with ones
-	t = np.linspace(0, duration, Fs * duration)
-	carrier = np.cos(2 * pi * fc * t)
+	'''
+	Modulates a binary array into an array that can be played
+	as an audible signal. Modulation done using Binary Phase-
+	Shift Keying.
 
+	binArray: a numpy array of binary bits
+	Fs: the sampling frequency measured in samples/second
+	fc: the frequency of the carrier wave, measured in Hz
+	bitTime: how long each bit is played for, measured in seconds
+
+	returns a numpy array that represents an audible signal
+	'''
+
+	numberOfBits = len(binArray)	# number of zeros and ones in array
+	duration = bitTime * numberOfBits	# time it takes to play the audio signal. measured in seconds
+	
+	#initialize arrays
+	signalArray = np.ones(int(duration * Fs))	# initialze the signal array
+	t = np.linspace(0, duration, Fs * duration)	# the time array
+	carrier = np.cos(2 * pi * fc * t)	# the carrier wave to be phase shifted
+
+	# encode the message
 	count = 0
-	bitLength = int(Fs * bitTime)	#number of data points representing each bit 
-	# create the square wave
+	bitLength = int(Fs * bitTime)	#number of points representing each bit 
+	# translate the bits into 1s and -1s
 	for i in binArray:
 		if i == 1:
 			amp = 1
@@ -31,12 +75,25 @@ def Modulate(binArray, Fs, fc, bitTime = .25):
 		secondIndex = (count + 1) * bitLength
 		signalArray[firstIndex:secondIndex] *= amp
 		count += 1
-	audioSignal = signalArray * carrier
+	# phase of carrier is shifted by pi at each sign change
+	# cos(x + pi) = -cos(x)
+	audioSignal = signalArray * carrier	
 	return audioSignal
 
 def PlayRecord(signalArray, Fs):
+	'''
+	Simultaneously plays an array representing an audio signal 
+	and records the resulting sound
+
+	signalArray: a numpy array representing an audio signal
+	Fs: the sampling frequency measured in samples per second
+
+	returns a numpy array of the recorded signal
+	'''
+
 	sd.default.samplerate = Fs
 	sd.default.channels = 1
+
 	return sd.playrec(signalArray, blocking=True)
 
 
@@ -45,21 +102,23 @@ def Demodulate(signalArray, Fs):
 	pass
 
 def BitsToString(binArray):
+	'''
+	Translates an array of binary bits to a string.
+
+	binArray: a numpy array of 1s and 0s. The number of
+	elements in the array must be a multiple of 8 in order to
+	represent complete 8 bit integers
+
+	returns a string
+	'''
 	asciiArray = np.packbits(binArray)
 	asciiList = asciiArray.tolist()
 	return ''.join(chr(i) for i in asciiList)
 
-def Transmit(Fs):
-	message = "Ariana"
-	binArray = StringToBits(message)
-	audioSignal = Modulate(binArray, Fs, 440)
-	sd.play(audioSignal, Fs)
 
 
 if __name__ == "__main__":
 	Fs = 44100
-	sd.default.samplerate = Fs
-	sd.default.channels = 1
 	message = StringToBits("A")
 	audioArray = Modulate(message, Fs, 440)
 	receivedArray = PlayRecord(audioArray, Fs)
